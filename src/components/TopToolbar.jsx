81f7f8577c24bef0081de1a471942df5b5b5b473
Hari2734 <harikrishnan.labglo@gmail.com>
2025-04-11 19:18:12 +0530

    adds header

--- src/components/TopToolbar.jsx
+++ src/components/TopToolbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/TopToolbar.css";
+import HeaderNav from "./HeaderNav";
 
 export default function TopToolbar() {
   return (
     <div className="top-toolbar">
-      
+      <HeaderNav/>
     </div>
   );
 }
